61fecf91e0b203e6e9dce2685ad3e87af853598c
Georgi Kalinkov <gkalinkov@Georgis-MacBook-Air.local>
2023-12-20 19:17:33 +0200

    Configure reverse proxy

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -1,4 +1,25 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("authenticated", policy =>
+        policy.RequireAuthenticatedUser());
+});
+
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
 
 builder.Services
     .AddReverseProxy()
@@ -7,6 +28,12 @@ builder.Services
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseRateLimiter();
 
 app.MapReverseProxy();
 
